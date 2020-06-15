d68a5cf879a11d591057200054dd6c9dfebcfcae
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2020-06-15 11:35:06 +0200

    add videos to report

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,19 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
+// Import commands.js using ES2015 syntax:
 import './commands'
+import addContext from 'mochawesome/addContext'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on("test:after:run", (test, runnable) => {
+    
+    let videoName = Cypress.spec.name
+    videoName = videoName.replace('/.js.*', '.js')
+    const videoUrl = 'videos/' + videoName + '.mp4'
+
+    addContext({ test }, videoUrl)
+});
+
